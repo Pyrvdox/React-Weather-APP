300a2730afcd38ef3735c0e4572fc53da0e089b6
Marcin Panek <evilmeryl@gmail.com>
2024-01-13 20:33:48 +0100

    Start app

---
+++ weatherapp/js/app.js
@@ -0,0 +1,1 @@
+const key = "b29e2d75-9512-4fcd-853d-22cc2220f5ea";
--- weatherapp/webpack.config.js
+++ weatherapp/webpack.config.js
@@ -1,47 +1,44 @@
-// const path = require("path");
-// const webpack = require("webpack");
-//
-// const entryPath = "weatherapp/src/index.js";
-//
-// module.exports = {
-//     mode: "none",
-//     entry: `./${entryPath}/js/app.js`,
-//     devtool: "inline-source-map",
-//     output: {
-//         filename: "out.js",
-//         path: path.resolve(__dirname, `${entryPath}/build`),
-//         clean: true,
-//     },
-//     devServer: {
-//         open: true,
-//         hot: true,
-//         static: [
-//             {
-//                 directory: path.join(__dirname, entryPath),
-//                 publicPath: "/",
-//                 serveIndex: true,
-//             },
-//         ],
-//         devMiddleware: {
-//             writeToDisk: true,
-//         },
-//         compress: true,
-//         port: 3001,
-//         historyApiFallback: true,
-//     },
-//     module: {
-//         rules: [
-//             {
-//                 test: /\.js$/,
-//                 exclude: /node_modules/,
-//                 loader: "babel-loader",
-//
-//             },
-//         ],
-//     },
-//     plugins: [
-//         new webpack.ProvidePlugin({
-//             process: "process/browser.js",
-//         }),
-//     ],
-// };
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+  mode: "none",
+  entry: `./js/app.js`,
+  devtool: "inline-source-map",
+  output: {
+    filename: "out.js",
+    path: path.resolve(__dirname, "build"),
+    clean: true,
+  },
+  devServer: {
+    open: true,
+    hot: true,
+    static: [
+      {
+        directory: path.join(__dirname),
+        publicPath: "/",
+        serveIndex: true,
+      },
+    ],
+    devMiddleware: {
+      writeToDisk: true,
+    },
+    compress: true,
+    port: 3001,
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+    ],
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      process: "process/browser",
+    }),
+  ],
+};
