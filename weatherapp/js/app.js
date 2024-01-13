0224d3eebdd56ad898b22d66836450f633ebe0eb
Pyrvdox <cierech8991@gmail.com>
2024-01-13 20:52:18 +0100

    react config

--- weatherapp/js/app.js
+++ weatherapp/js/app.js
@@ -1,1 +1,15 @@
+import React from "react";
+import {createRoot} from "react-dom/client";
+
 const key = "b29e2d75-9512-4fcd-853d-22cc2220f5ea";
+
+const App = () => {
+    return (
+        <div><h1>Hello World</h1></div>
+    )
+}
+
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App />);
